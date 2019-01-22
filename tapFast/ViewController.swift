72a661c656c066089b4647bad278d2e0759f9d0d
Ievgeniia Pavliuchyk <ievgeniia.pavliuchyk@gmail.com>
2019-01-22 22:48:05 +0100

    Initial Commit

---
+++ tapFast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tapFast
+//
+//  Created by Ievgeniia Pavliuchyk on 22/01/2019.
+//  Copyright © 2019 Ievgeniia Pavliuchyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
