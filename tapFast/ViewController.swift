864c893b7a70e699070ae0dafdd1ffb0f8f47907
Ievgeniia Pavliuchyk <ievgeniia.pavliuchyk@gmail.com>
2019-01-24 00:20:50 +0100

    First Commit of "Tap me fast" game with universal UI, that works on various devices and ability to start timer countdown

--- tapFast/ViewController.swift
+++ tapFast/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var startGameButton: UIButton!
+    
+    var timer = Timer()
+    
+    var timeInt = 10
+    var scoreInt = 0
+    var gameInt = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timeInt = 10
     }
 
+    @IBAction func startGame(_ sender: Any) {
+        
+        if timeInt == 10 {
+            
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(decreaseTimer ), userInfo: nil, repeats: true)
+    
+        }
+        
+    }
+    
+    @objc func decreaseTimer(){
+        
+        timeInt -= 1
+        timeLabel.text = String(timeInt)
+        
+    }
 
+   
+    
 }
 
